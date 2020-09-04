7e2a1b734dd65d70092216afbdea2902cb7fd643
JossinAntony <jossinde@gmail.com>
2020-09-04 14:18:33 +0200

    searchBooks.dart added.

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bookstore_app/pages/addBooks.dart';
+import 'package:flutter_bookstore_app/pages/searchBooks.dart';
+
 
 class Menu extends StatelessWidget {
 
