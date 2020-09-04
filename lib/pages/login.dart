fa4f242036a53eeea782a0d7e9338b46e710a5cc
JossinAntony <jossinde@gmail.com>
2020-09-04 08:46:05 +0200

    login field super container set.

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,22 +9,41 @@ class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('BOOKSTORE'),
-          centerTitle: true,
-          backgroundColor: Colors.blueGrey,
-        ),
-        body: SizedBox.expand(
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [Color(0xff67B26F), Color(0xff4ca2cd),],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('BOOKSTORE'),
+              centerTitle: true,
+              backgroundColor: Colors.blueGrey,
             ),
-          ),
-        )
-      )
-    );
+            body: SizedBox.expand(
+              child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      Color(0xff67B26F),
+                      Color(0xff4ca2cd),
+                    ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.25,
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.blueGrey,
+                          width: 1.5,
+                          style: BorderStyle.solid,
+                        ),
+                        borderRadius: BorderRadius.circular(12)),
+                        child: Column(
+                          children: <Widget>[
+                            Text('HI'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )),
+            )));
   }
 }
